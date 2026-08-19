127ccd00d0d7d96ba3332dca01199c4be38d50cf
alsonmachingauta06-lab <alsonmachingauta06@gmail.com>
2026-08-19 12:34:18 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,18 @@
+const settings = {
+  packname: 'Alson XMD',
+  author: '‎ALSON MACHINGAUTA',
+  botName: "Alson XMD",
+  botOwner: 'Alson Machingauta', // Your name
+  ownerNumber: '263786359833', //Set your number here without + symbol, just add country code & number without any space
+  ytChannel: 'https://whatsapp.com/channel/', // Alson MD Channel
+ytApiKey: '', // Leave empty to disable YT command
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/alsonmachingauta06-lab/Alson XMD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
