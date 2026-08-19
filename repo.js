c41f01ea9bd240bfda3c7472fc951719c141d514
alsonmachingauta06-lab <alsonmachingauta06@gmail.com>
2026-08-19 13:05:22 +0200

    Update repository link in repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-conn.reply(m.chat, `*ALSON XMD BOT*\n\n*Repository:* https://github.com/alsonmachingauta06-lab/Alson-XMD-\n\n*Owner:* ${global.ownerName}`, m)
+conn.reply(m.chat, `*ALSON XMD BOT*\n\n*Repository:* https://github.com/alsonmachingauta06-lab/Elson\n\n*Owner:* ${global.ownerName}`, m)
 }
 handler.command = ['repo', 'sc', 'source']
 handler.help = ['repo']
