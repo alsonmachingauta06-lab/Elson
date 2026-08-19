41a3f8a7e5adc27d629e8f2032e7dec7058dabaf
alsonmachingauta06-lab <alsonmachingauta06@gmail.com>
2026-08-19 12:45:17 +0200

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,18 +1,12 @@
-const settings = {
-  packname: 'Alson XMD',
-  author: '‎ALSON MACHINGAUTA',
-  botName: "Alson XMD",
-  botOwner: 'Alson Machingauta', // Your name
-  ownerNumber: '263786359833', //Set your number here without + symbol, just add country code & number without any space
-  ytChannel: 'https://whatsapp.com/channel/', // Alson MD Channel
-ytApiKey: '', // Leave empty to disable YT command
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  maxStoreMessages: 20, 
-  storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.7",
-  updateZipUrl: "https://github.com/alsonmachingauta06-lab/Alson XMD/archive/refs/heads/main.zip",
-};
+global.owner = ['263786359833'] // put your whatsapp number with country code, no +
+global.botName = 'Alson XMD'
+global.ownerName = 'Alson'
 
-module.exports = settings;
+global.repo = 'https://github.com/alsonmachingauta06-lab/Alson-XMD-'
+global.github = 'https://github.com/alsonmachingauta06-lab/Alson-XMD-'
+global.channelLink = '' // leave empty so it doesn't redirect
+
+global.packname = 'Alson XMD'
+global.author = 'Alson Machingauta'
+
+module.exports = global
